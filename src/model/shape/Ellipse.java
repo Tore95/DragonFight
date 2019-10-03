ff5a4c59dd0e57e0b5ce72de48a57e7755f683ac
Tore95 <43842155+Tore95@users.noreply.github.com>
2019-10-03 18:35:19 +0200

    Added shapes classes for refactoring the hitbox

---
+++ src/model/shape/Ellipse.java
@@ -0,0 +1,49 @@
+package model.shape;
+
+public class Ellipse {
+
+    private static double[] radians = {
+        0d,
+        Math.PI / 4d,
+        Math.PI / 2d,
+        (3d * Math.PI) / 4d,
+        Math.PI,
+        (5d * Math.PI) / 4d,
+        (3d/2d) * Math.PI,
+        (7d/4d) * Math.PI
+    };
+
+    private Point center;
+    private double a;
+    private double b;
+
+    public Ellipse(double xc, double yc, double width, double height) {
+        center = new Point(xc,yc);
+        this.a = width / 2;
+        this.b = height / 2;
+    }
+
+    private boolean isInside(Point p) {
+        return (
+            (Math.pow(p.x - center.x,2)/Math.pow(a,2)) +
+            (Math.pow(p.y - center.y,2)/Math.pow(b,2))
+        ) <= 1;
+    }
+
+    public Point[] getPoints() {
+        Point[] points = new Point[8];
+        for (int i = 0; i < points.length; i++) {
+            points[i].x = (Math.cos(radians[i]) * a) + center.x;
+            points[i].y = (Math.sin(radians[i]) * b) + center.y;
+        }
+        return points;
+    }
+
+    public boolean intersect(Ellipse e) {
+        Point[] points = e.getPoints();
+        for (Point p: points) {
+            if (isInside(p)) return true;
+        }
+        return false;
+    }
+}
---
+++ src/model/shape/Point.java
@@ -0,0 +1,25 @@
+package model.shape;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this(0,0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        Point p = (Point) obj;
+        return this.x == p.x && this.y == p.y;
+    }
+}
